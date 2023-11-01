b2acdf1e02eb8de52a1926e9ccb71b254b997a89
ykwizera <y.kwizera@alustudent.com>
2023-11-01 11:25:48 -0700

    upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,4 @@ def uppercase(input_str):
             result += chr(ord(char) - 32)
         else:
             result += char
-    print("{}".format(result), end = "\n")
+    print("{}".format(result), end="\n")
