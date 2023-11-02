8922f8b88ec01cb39f775a701419ffc745d0b481
ykwizera <y.kwizera@alustudent.com>
2023-11-02 09:46:30 -0700

    sys

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,19 +6,19 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-    if operator == '+':
+
+    a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+
+    if operator == "+":
         result = add(a, b)
-    elif operator == '-':
+    elif operator == "-":
         result = sub(a, b)
-    elif operator == '*':
+    elif operator == "*":
         result = mul(a, b)
-    elif operator == '/':
+    elif operator == "/":
         result = div(a, b)
     else:
-        print("Unknown operator. Available operators: +, -, *, /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
