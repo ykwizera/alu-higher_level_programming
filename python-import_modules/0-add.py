e469bafe7eeddc5309114872d268f69cdfb70db6
ykwizera <y.kwizera@alustudent.com>
2023-11-02 01:17:52 -0700

    modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ a = 1
 b = 2
 if __name__ == "__main__":
     from add_0 import add
-    print("{} + {} = {}".format(a, b, add(a,b)), end=' \n')
+    print("{} + {} = {}".format(a, b, add(a,b)), end='\n')
