223bd7667e11be69b9bed482c8645fb45fc09e41
ykwizera <y.kwizera@alustudent.com>
2023-11-02 02:50:12 -0700

    len

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
     num_args = len(argv)
-    print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''))
+    print("{} argument{}{}".format(num_args, 's' if num_args != 1 else '', '.' if num_args == 0 else ':'))
     for i, arg in enumerate(argv, 1):
         print("{}: {}".format(i, arg))
