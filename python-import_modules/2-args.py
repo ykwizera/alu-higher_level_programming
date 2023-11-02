3f9d39694f7cd2372bfebd085f526c0991a63279
ykwizera <y.kwizera@alustudent.com>
2023-11-02 02:43:22 -0700

    len

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main":
+if __name__ == "__main__":
     argv = sys.argv[1:]
     num_args = len(argv)
     print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''))
