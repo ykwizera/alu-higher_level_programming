e168c3aecfd3c1b6402d3344d6ec3322c20a0fb0
ykwizera <y.kwizera@alustudent.com>
2023-11-02 03:29:22 -0700

    len

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     total = 0
     for arg in argv:
-        total += int(arg)
-        print(total)
+        total = total + int(arg)
+
+    print(total)
 
