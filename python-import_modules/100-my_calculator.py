58a7f2cb401a17665fa36044540078e045e5db25
ykwizera <y.kwizera@alustudent.com>
2023-11-02 09:39:33 -0700

    sys

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
