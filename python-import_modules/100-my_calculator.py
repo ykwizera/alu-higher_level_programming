8f415285000a2b365612ebfdfbf28714000c2f7b
ykwizera <y.kwizera@alustudent.com>
2023-11-03 03:06:35 -0700

    calc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, mul, div
+import calculator_1
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
     if operator == '+':
-        result = add(a, b)
+        result = calculator_1.add(a, b)
     elif operator == '-':
-        result = sub(a, b)
+        result = calculator_1.sub(a, b)
     elif operator == '*':
-        result = mul(a, b)
+        result = calculator_1.mul(a, b)
     elif operator == '/':
         if b == 0:
             print("Error: Division by zero")
             sys.exit(1)
-        result = div(a, b)
+        result = calculator_1.div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
