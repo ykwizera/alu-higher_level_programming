619608376d78ca29ef43511f3bdb03285fdc3567
ykwizera <y.kwizera@alustudent.com>
2023-11-07 07:20:01 -0800

    tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    x, z = tuple_a[0] if len(tuple_a) >= 1 else 0, tuple_a[1] if len(tuple_a) >= 2 else 0
-    u, v = tuple_b[0] if len(tuple_b) >= 1 else 0, tuple_b[1] if len(tuple_b) >= 2 else 0
+    x = tuple_a[0] if len(tuple_a) >= 1 else 0
+    z = tuple_a[1] if len(tuple_a) >= 2 else 0
+    u = tuple_b[0] if len(tuple_b) >= 1 else 0
+    v = tuple_b[1] if len(tuple_b) >= 2 else 0
     result = (int(x) + int(u), int(z) + int(v))
     return result
