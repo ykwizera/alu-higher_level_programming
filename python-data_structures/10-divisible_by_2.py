0c727186a5e32ebe30972eab302e3a70fc7bbb74
ykwizera <y.kwizera@alustudent.com>
2023-11-07 08:23:20 -0800

    2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result = []
+    for num in my_list:
+        is_multiple_of_2 = (num % 2 == 0)
+        result.append(is_multiple_of_2)
+    return result
