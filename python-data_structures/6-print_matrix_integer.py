6886b5b0a01decf75332c4e054edf327c357a1f1
ykwizera <y.kwizera@alustudent.com>
2023-11-07 01:53:19 -0800

    matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print(" ")
+        return
+    for row in matrix:
+        for i, element in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d} ".format(element), end='')
+            else:
+                print("{:d}".format(element))
+    if not matrix if matrix is None:
+        print(" ")
+        return:
