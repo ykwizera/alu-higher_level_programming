6fe95f7e18152b749195208239e9e49294934720
ykwizera <y.kwizera@alustudent.com>
2023-11-07 08:04:03 -0800

    max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,4 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    return max(my_list)
+    biggest = my_list[0]
+    for num in my_list:
+        if num > biggest:
+            biggest = num
+    return biggest
