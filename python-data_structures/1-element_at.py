a9680485175942b5f031606522a5b3c088a1e1e2
ykwizera <y.kwizera@alustudent.com>
2023-11-06 23:47:21 -0800

    list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        print("none")
+        print("None")
     else:
         x = my_list[idx]
         return x
