f43ba0ce7c254cd0c7323b745f488b638b9428b7
ykwizera <y.kwizera@alustudent.com>
2023-11-08 12:15:26 -0800

    roman numbers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_symbols = {
+    'I' : 1,
+    'V' : 5,
+    'X' : 10,
+    'L' : 50,
+    'C' : 100,
+    'D' : 500,
+    'M' : 1000
+    }
+    sum_r = 0
+    var = 0
+    for symbol in reversed(roman_string):
+        value = roman_symbols.get(symbol)
+        if value is None:
+            return 0
+        if value < var:
+            sum_r -= value
+        else:
+            sum_r += value
+        var = value
+    return sum_r
