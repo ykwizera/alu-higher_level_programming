4c3d2fe85cf9f4ffffd5165bd351caee0830a29e
ykwizera <y.kwizera@alustudent.com>
2023-11-08 08:35:03 -0800

    sorted keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    result = sorted(a_dictionary.items())
-    sort = {}
-    for key,value in result:
-        sort[key] = value
-    return sort
+    result = sorted(a_dictionary.keys())
+    for key in result:
+        print(f"{key}: {a_dictionary[key]}")
+    return result
