026810af9368ea384c6fb3612e67c69c099e3c26
ykwizera <y.kwizera@alustudent.com>
2023-11-08 00:03:25 -0800

    square matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        print(" ")
+        return
+    elif not matrix or all(not row for row in matrix):
+        print()
+    else:
+        new_matrix = []
+        for row in matrix:
+            new_row = []
+            for num in row:
+                new_row.append(num ** 2)
+            new_matrix.append(new_row)
+        return new_matrix
