fb974dbe3f722cfecbb1db3cad9314a252130ef5
ykwizera <y.kwizera@alustudent.com>
2023-11-09 00:32:43 -0800

    listing

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,16 @@ def safe_print_list_integers(my_list=[], x=0):
     printed_integers = 0
 
     try:
-        for i in range(x):
+        i = 0
+        while True:
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 printed_integers += 1
-        print()
-    except IndexError:
+            i += 1
+            if printed_integers >= x:
+                break
+    except (IndexError, TypeError):
         pass
 
+    print()
     return printed_integers
