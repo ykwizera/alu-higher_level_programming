a0be282ed43c7d013f4751b1be171b2287a8e60a
ykwizera <y.kwizera@alustudent.com>
2023-11-09 00:25:32 -0800

    listing

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_integers = 0
+
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                printed_integers += 1
+        print()
+    except IndexError:
+        pass
+
+    return printed_integers
