08354b546d5c8f565132bfc8df4f93d1e901085c
ykwizera <y.kwizera@alustudent.com>
2023-11-15 00:35:26 -0800

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)
-    pass
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    def sides(self):
+        python3 -c 'print(__import__("my_module").__doc__)'
