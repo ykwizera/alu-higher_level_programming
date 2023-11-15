e5ac9580bad4d42d82b25803f83448dc806fa2dc
ykwizera <y.kwizera@alustudent.com>
2023-11-15 03:28:20 -0800

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 class Square:
     """
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    python3 -c 'print(__import__("sides").Square.__doc__)'
     """
     def sides(self):
         """
-        python3 -c 'print(__import__("my_module").__doc__)'
-        python3 -c 'print(__import__("my_module").my_function.__doc__)'
-        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        python3 -c 'print(__import__("sides").__doc__)'
+        python3 -c 'print(__import__("sides").sides.__doc__)'
+        python3 -c 'print(__import__("my_"(sides").Square.sides.__doc__)'
         """
