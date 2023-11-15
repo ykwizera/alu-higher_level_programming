33bfb6d75a400ea084d6fb8f3863cd119e1045c7
ykwizera <y.kwizera@alustudent.com>
2023-11-15 03:43:54 -0800

    2 suare

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 1_suare.py
+
+"""
+this defines a suare by size
+"""
+
+class Square:
+    """
+    This is square class and we're adding a private 
+    attribute size
+    """
+    __size = None
+    def __init__(self)
+    """
+    __init__
+    """
