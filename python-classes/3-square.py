ad8aa8b400a7967ce37f27063bb8b33b47a3c4d9
ykwizera <y.kwizera@alustudent.com>
2023-11-15 23:49:25 -0800

    area

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 3-square.py
+
+
+"""
+This is my module
+"""
+
+
+class Square:
+    """
+    This is my class
+    """
+    def __init__(self, size=0):
+        """
+        This is y init module
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """
+        calculatung the area using size
+        """
+        print("Area: {}".format(self.__size * self.__size))
