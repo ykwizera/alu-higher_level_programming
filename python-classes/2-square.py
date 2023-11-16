bebb709f3460785b5e8fd5847eae6e89219c5aaf
ykwizera <y.kwizera@alustudent.com>
2023-11-15 23:38:38 -0800

    errors

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 2-square.py
+
+
+"""
+This is my module
+"""
+
+class Square:
+    """
+    This is my class
+    """
+    def __init__(self, size=0):
+        """
+        This is my init function
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
