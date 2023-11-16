59f839a34baade915c480d4f3cd6af97f9d47c3e
ykwizera <y.kwizera@alustudent.com>
2023-11-16 00:25:01 -0800

    getters and setters

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# 4-square.py
+
+
+"""
+This is my module
+"""
+
+
+class Square:
+    def get_size(self):
+        """
+        get function
+        """
+        return self.__size
+    def set_size(self, size):
+        """
+        set module
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def __init__(self, size=0):
+        """
+        initialising size
+        """
+        self.set_size(size)
+    def area(self):
+        """
+        Calculating the area using size
+        """
+        return self.__size * self.__size
