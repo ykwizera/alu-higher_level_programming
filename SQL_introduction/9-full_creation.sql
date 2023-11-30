065d53d36e34a0468005d1a78d1eee20470650fa
ykwizera <y.kwizera@alustudent.com>
2023-11-29 22:54:14 -0800

    full table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name  VARCHAR(256),
 	score int
 	);
-INSERT INTO second_table VALUES(1, "John", 10)
-INSERT INTO second_table VALUES(2, "Alex", 3)
-INSERT INTO second_table VALUES(3, "Bob", 14)
-INSERT INTO second_table VALUES(4, "George", 8)
+INSERT INTO second_table(id, name, score)
+VALUES  (1, 'John', 10),
+        (2, 'Alex', 3),
+        (3, 'Bob', 14),
+        (4, 'George', 8);
