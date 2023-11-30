d9d495e73970df1e810ffa86b8144d2dca683f2e
ykwizera <y.kwizera@alustudent.com>
2023-11-29 22:52:12 -0800

    full table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creating a full table
+CREATE TABLE IF NOT EXISTS second_table(
+	id    int,
+	name  VARCHAR(256),
+	score int
+	);
+INSERT INTO second_table VALUES(1, "John", 10)
+INSERT INTO second_table VALUES(2, "Alex", 3)
+INSERT INTO second_table VALUES(3, "Bob", 14)
+INSERT INTO second_table VALUES(4, "George", 8)
