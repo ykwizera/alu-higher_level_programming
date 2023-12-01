96608067d75b665f66b2ea7cd5203e6ab38bb5ca
ykwizera <y.kwizera@alustudent.com>
2023-11-30 21:59:32 -0800

    1

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
-"""Documentation"""
-
 import urllib.request
 import urllib.error
 import sys
 
-if __name__ == '__main__':
-    """"Documented"""
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
+def fetch_url_content(url):
     try:
-        with urllib.request.urlopen(req) as response:
-            content = response.read()
-            print("{}".format(content.decode("utf-8")))
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-    except urllib.error.URLError as e:
-        print(e.reason)
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./3-error_code.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    fetch_url_content(url
