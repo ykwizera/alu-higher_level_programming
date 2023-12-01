b596a31f76ae9d647e05fccf106cfbefb8d30843
ykwizera <y.kwizera@alustudent.com>
2023-11-30 23:37:24 -0800

    1

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-# Module 1: urllib.request
+"""send request."""
 import urllib.request
 import urllib.error
 import sys
-def fetch_url_content(url):
-    #function
+
+
+def send_request():
+    """Send request."""
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        with urllib.request.urlopen(sys.argv[1]) as re:
+            xyz = re.read()
+            print(xyz.decode("utf-8"))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
+
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided.
-    if len(sys.argv) != 2:
-        print("Usage: ./3-error_code.py <URL>")
-        sys.exit(1)
-
-    # Extract the URL from the command-line argument.
-    url = sys.argv[1]
-
-    # Call the fetch_url_content function with the provided URL.
-    fetch_url_content(url)
+    send_request()
