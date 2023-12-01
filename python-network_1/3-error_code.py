faf0727d779a329045e7feaf043afecb9034a15f
ykwizera <y.kwizera@alustudent.com>
2023-11-30 23:47:49 -0800

    1

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Documentation"""
-
-import urllib.request
-import urllib.error
+"""
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
+"""
+from urllib import request, error
 import sys
 
-if __name__ == '__main__':
-    """"Documented"""
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
+
+if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(req) as response:
-            content = response.read()
-            print("{}".format(content.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-    except urllib.error.URLError as e:
-        print(e.reason)
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
