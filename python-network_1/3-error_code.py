a07a1df3694316c00cd8b954b499a9289c225a44
ykwizera <y.kwizera@alustudent.com>
2023-12-01 09:41:30 -0800

    1

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request and displays
-the value of the X-Request-Id variable found in the header
-(handling HTTP errors)
-"""
-from urllib import request, error
+
 import sys
+from urllib import request, error
 
-
-if __name__ == "__main__":
+def fetch_request_id(url):
     try:
-        with request.urlopen(sys.argv[1]) as response:
-            body = response.read()
-            print(body.decode('utf-8'))
+        with request.urlopen(url) as response:
+            request_id = response.headers.get('X-Request-Id')
+            if request_id:
+                return request_id
+            else:
+                print("Error: No X-Request-Id header found")
+                return None
     except error.HTTPError as err:
         print('Error code: {}'.format(err.code))
+        return None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <url>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    request_id = fetch_request_id(url)
+
+    if request_id:
+        print("X-Request-Id: {}".format(request_id))
