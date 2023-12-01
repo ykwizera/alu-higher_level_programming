e2fadd31b214f2f20fc37c05f539946451a511ee
ykwizera <y.kwizera@alustudent.com>
2023-12-01 05:09:41 +0000

    1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Documented"""
-
+"""
+Fetches https://alu-intranet.hbtn.io/status using urllib
+"""
 
 import urllib.request
 
+url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
