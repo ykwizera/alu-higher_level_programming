6dfe1f9ef789053ca7dbdbac16cf534c85508f20
ykwizera <y.kwizera@alustudent.com>
2024-02-04 04:32:30 -0800

    js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const [, , ...args] = process.argv;
+if (Number(args[0])) {
+  const num = parseFloat(args[0]);
+  let a = num.toFixed(0);
+  for (let i = 0; i < a; i++) {
+    let y = '';
+    for (let z = 0; z < a; z++) {
+      y += 'X';
+    }
+    console.log(y);
+  }
+} else {
+  console.log('Missing size');
+}
+
