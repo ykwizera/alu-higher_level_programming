2b6c92cb4ede8e5ba765feb52b248495d559e987
ykwizera <y.kwizera@alustudent.com>
2024-02-04 00:19:29 -0800

    js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -5,9 +5,9 @@ const [, , ...args] = process.argv;
 if (args[1]) {
 	console.log(args[0], 'is', args[1]);
 } else if (args.length === 1) {
-	console.log(args[0], 'is  undefined');
+	console.log(args[0], 'is undefined');
 } else if (args.length === 0) {
-	console.log('undefined is  undefined');
+	console.log('undefined is undefined');
 } else {
 	console.log(' ');
 }
