3483143502aaf68862c25fb6d1b60d111c3085f8
ykwizera <y.kwizera@alustudent.com>
2024-02-04 03:55:52 -0800

    js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const [, , ...args] = process.argv;
+
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -7,5 +7,5 @@ if (Number(args[0])) {
     console.log('C is fun');
   }
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
